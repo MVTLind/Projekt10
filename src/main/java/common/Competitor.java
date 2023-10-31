19702682fbf809e381f1b6d6f1570249394aa959
Wilmer Ljungberg <wilmerljungberg01@gmail.com>
2023-10-31 13:51:11 +0100

    Added Competitor.java

---
+++ src/main/java/common/Competitor.java
@@ -0,0 +1,13 @@
+package common;
+
+import decathlon.Deca100M;
+
+public class Competitor {
+
+    Integer Deca100M = 0;
+    Integer Deca110mHurdles = 0;
+    Integer Deca400M = 0;
+    Integer Deca1500M = 0;
+    Integer DecaDiscusThrow = 0;
+    Integer DecaHighJump = 0;
+}
