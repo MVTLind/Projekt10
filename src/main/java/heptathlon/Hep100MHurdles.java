df80293382b0ab2bcc04edab54098de11d23ae99
MVTLind <ebba.lindskog@gmail.com>
2023-11-07 11:19:38 +0100

    Bug nr1 fix, uncommented the code, it no longer loops

--- src/main/java/heptathlon/Hep100MHurdles.java
+++ src/main/java/heptathlon/Hep100MHurdles.java
@@ -25,6 +25,7 @@ public class Hep100MHurdles {
 					runningTime = inputResult.enterResult();
 				} else if (runningTime > 26.4) {
 					System.out.println("Value too high");
+					runningTime = inputResult.enterResult();
 					//runningTime = inputResult.enterResult();
 				} else {
 
